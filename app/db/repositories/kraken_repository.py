646cc0626de081b19d0f96670ee2263a40bddcdf
Davi Souza <davi.souza@upperds.com>
2022-03-26 14:35:30 -0300

    mudando função para o local certo

--- app/db/repositories/kraken_repository.py
+++ app/db/repositories/kraken_repository.py
@@ -11,6 +11,17 @@ class KrakenRepository(BaseRepository[Kraken]):
     def __init__(self, uow: UnitOfWork) -> None:
         super().__init__(uow, Kraken)
 
+    async def update_status(self, kraken_model: Kraken) -> int:
+        qb = sa.select(Kraken).where(Kraken.id == kraken_model.id)
+        result = await self.uow.session.execute(qb)
+        data = result.scalars().first()
+
+        async with self.uow as uow:
+            data.post_status = kraken_model.post_status
+            await uow.session.commit()
+
+        return kraken_model.id
+
     async def get_queue_posts(self) -> List[Kraken]:
         qb = sa.select(Kraken).options(joinedload(Kraken.twitch_clips))
 
--- app/db/repositories/twitch_repository.py
+++ app/db/repositories/twitch_repository.py
@@ -12,17 +12,6 @@ class TwitchRepository(BaseRepository[TwitchClips]):
     def __init__(self, uow: UnitOfWork) -> None:
         super().__init__(uow, TwitchClips)
 
-    async def update_status(self, twitch_model: TwitchClips) -> int:
-        qb = sa.select(TwitchClips).where(TwitchClips.id == twitch_model.id)
-        result = await self.uow.session.execute(qb)
-        data = result.scalars().first()
-
-        async with self.uow as uow:
-            data.post_status = twitch_model.post_status
-            await uow.session.commit()
-
-        return twitch_model.id
-
     async def get_twitch_clips_by_clip_url(self, clip_url: str) -> TwitchClips:
         qb = sa.select(TwitchClips).where(TwitchClips.clip_url == clip_url)
         result = await self.uow.session.execute(qb)
