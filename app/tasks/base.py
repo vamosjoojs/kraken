0b3d64e17d5ca335328f8771e0c7f71f419632a2
Davi Souza <davi.souza@upperds.com>
2022-03-26 15:03:09 -0300

    testando abertura do db com dispose no final de cada tasks

--- app/tasks/base.py
+++ app/tasks/base.py
@@ -9,30 +9,34 @@ from app.api.dependencies.get_db import engine
 
 
 class DatabaseTask(Task):
-    _kraken_repository = None
-    _twitch_repository = None
-    _auto_tasks_repository = None
+    _session = None
+    _engine = engine
+
+    def after_return(self, *args, **kwargs):
+        if self._session is not None:
+            await self._session.close()
+            await self._engine.dispose()
 
     @property
     def kraken_repository(self):
-        if self._kraken_repository is None:
-            session = AsyncSession(engine, expire_on_commit=False)
+        if self._session is None:
+            session = AsyncSession(self._engine, expire_on_commit=False)
             uow = UnitOfWork(session)
             self._kraken_repository = KrakenRepository(uow)
         return self._kraken_repository
 
     @property
     def twitch_repository(self):
-        if self._twitch_repository is None:
-            session = AsyncSession(engine, expire_on_commit=False)
+        if self._session is None:
+            session = AsyncSession(self._engine, expire_on_commit=False)
             uow = UnitOfWork(session)
             self._twitch_repository = TwitchRepository(uow)
         return self._twitch_repository
 
     @property
     def auto_tasks_repository(self):
-        if self._auto_tasks_repository is None:
-            session = AsyncSession(engine, expire_on_commit=False)
+        if self._session is None:
+            session = AsyncSession(self._engine, expire_on_commit=False)
             uow = UnitOfWork(session)
             self._auto_tasks_repository = AutoTasksRepository(uow)
         return self._auto_tasks_repository
