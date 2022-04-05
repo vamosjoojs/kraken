2fb9ad4e49f2ca75c67db1ef2db918e73bf3810e
Davi Souza <davi.souza@upperds.com>
2022-04-05 10:14:56 -0300

    getdb close connection

--- app/api/dependencies/get_db.py
+++ app/api/dependencies/get_db.py
@@ -1,15 +1,19 @@
+from collections import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+
 from app.config.config import config
 from app.db.uow import UnitOfWork
 
 engine = create_async_engine(config.DATABASE_URI_ASYNC, echo=True, future=True)
 
+SessionLocal = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession, future=True
+)
 
-async def get_uow():
-    session = AsyncSession(engine, expire_on_commit=False)
-    try:
+
+async def get_uow() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
         yield UnitOfWork(session)
-    finally:
-        await session.close()
-        await engine.dispose()
