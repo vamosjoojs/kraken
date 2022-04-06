64cc02a67d5a5c15b36504152159c8e61abfceaf
Davi Souza <davi.souza@upperds.com>
2022-04-06 13:55:07 -0300

    beat com o worker

--- Procfile
+++ Procfile
@@ -1,5 +1,4 @@
-worker: celery --app celery_app.app worker -E -l info
-beat: celery --app celery_app.app beat
+worker: celery --app celery_app.app worker -B -E -l info
 web: uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-8000}
 
 
--- celery_app.py
+++ celery_app.py
@@ -1,24 +1,16 @@
 from celery import Celery
 from celery.schedules import crontab
-from kombu import Queue
 from app.config.config import config
-
-# celery_queues = {"default": ("kraken", 2)}
 
 app = Celery("broker", include=['app.tasks'])
 
-# app.conf.task_acks_late = True
-# app.conf.task_queues = (
-#     Queue(q, routing_key="broker.#") for q, w in celery_queues.values()
-# )
+
 app.conf.update(broker_url=config.REDIS_URL,
                 result_backend=config.REDIS_URL)
 
 app.conf.beat_schedule = {
     'send-messages': {
         'task': 'app.tasks.tasks.twitter_send_message',
-        'schedule': crontab(hour='*/2')
+        'schedule': crontab(minute=0, hour='*/2')
     },
 }
-
-# app.conf.task_default_queue = celery_queues["default"][0]
