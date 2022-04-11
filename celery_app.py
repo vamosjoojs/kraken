7e4ffad71f460f7de44d78681728f2db23076936
Davi Souza <davi.souza@upperds.com>
2022-04-11 11:39:06 -0300

    removendo beat scheduler

--- celery_app.py
+++ celery_app.py
@@ -7,10 +7,3 @@ app = Celery("broker", include=['app.tasks'])
 
 app.conf.update(broker_url=config.REDIS_URL,
                 result_backend=config.REDIS_URL)
-
-app.conf.beat_schedule = {
-    'send-messages': {
-        'task': 'app.tasks.tasks.twitter_send_message',
-        'schedule': crontab(minute=0, hour='*/2')
-    },
-}
